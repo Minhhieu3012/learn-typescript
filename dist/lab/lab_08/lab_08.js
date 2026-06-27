f9ba454a9065fb1e0289ab3d88db64849b0869f3
Minhhieu3012 <mjnkhieu3012@gmail.com>
2026-06-28 01:10:49 +0700

    update: lab 08

--- dist/lab/lab_08/lab_08.js
+++ dist/lab/lab_08/lab_08.js
@@ -1,2 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(name, email, role) {
+        this.name = name;
+        this._email = email;
+        this.role = role ?? "student";
+    }
+    get email() {
+        return this._email;
+    }
+    set email(value) {
+        this._email = value;
+    }
+    printUserInfo() {
+        console.log(`[GET INFO]
+        name: ${this.name}
+        email: ${this._email}
+        role: ${this.role}
+    `);
+    }
+}
+class Teacher extends User {
+    constructor(name, email, role) {
+        super(name, email, role);
+        this.courses = []; //[js,java,typeScript,..]
+    }
+    addCourse(courseName) {
+        this.courses.push(courseName);
+    }
+    printUserInfo() {
+        const courseName = this.courses.join(","); //js,java,...
+        console.log(`[${this.role}] ${this.name} - Courses Taught: ${courseName} `);
+    }
+}
+class Student extends User {
+    constructor(name, email) {
+        super(name, email);
+        this.enrolledCourses = [];
+    }
+    enroll(courseName) {
+        this.enrolledCourses.push(courseName);
+    }
+    printUserInfo() {
+        const courseName = this.enrolledCourses.join(",");
+        console.log(`[${this.role}] ${this.name} - Enrolled Courses: ${courseName}`);
+    }
+}
+const ohito = new Student("Ohito", "ohito@email.com");
+ohito.enroll("TypeScript");
+ohito.enroll("Node JS");
+const hieu = new Teacher("Hieu", "hieu@email.com", "teacher");
+hieu.addCourse("React");
+hieu.addCourse("TypeScript");
+ohito.printUserInfo();
+hieu.printUserInfo();
--- src/lab/lab_08/lab_08.ts
+++ src/lab/lab_08/lab_08.ts
@@ -1,1 +1,80 @@
 export {};
+
+type TRole = "student" | "teacher";
+
+class User {
+  public name: string;
+
+  private _email: string;
+
+  protected role: TRole;
+
+  constructor(name: string, email: string, role?: TRole) {
+    this.name = name;
+    this._email = email;
+    this.role = role ?? "student";
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+  public set email(value: string) {
+    this._email = value;
+  }
+
+  printUserInfo() {
+    console.log(
+      `[GET INFO]
+        name: ${this.name}
+        email: ${this._email}
+        role: ${this.role}
+    `,
+    );
+  }
+}
+
+class Teacher extends User {
+  courses: string[] = []; //[js,java,typeScript,..]
+
+  constructor(name: string, email: string, role: TRole) {
+    super(name, email, role);
+  }
+
+  addCourse(courseName: string) {
+    this.courses.push(courseName);
+  }
+
+  printUserInfo() {
+    const courseName = this.courses.join(","); //js,java,...
+    console.log(`[${this.role}] ${this.name} - Courses Taught: ${courseName} `);
+  }
+}
+
+class Student extends User {
+  enrolledCourses: string[] = [];
+
+  constructor(name: string, email: string) {
+    super(name, email);
+  }
+
+  enroll(courseName: string) {
+    this.enrolledCourses.push(courseName);
+  }
+
+  printUserInfo() {
+    const courseName = this.enrolledCourses.join(",");
+    console.log(`[${this.role}] ${this.name} - Enrolled Courses: ${courseName}`);
+  }
+}
+
+const ohito = new Student("Ohito", "ohito@email.com");
+ohito.enroll("TypeScript");
+ohito.enroll("Node JS");
+
+const hieu = new Teacher("Hieu", "hieu@email.com", "teacher");
+hieu.addCourse("React");
+hieu.addCourse("TypeScript");
+
+ohito.printUserInfo();
+
+hieu.printUserInfo();
