01b0f553292a6ccd78bfe44a2713c26216f028db
Minhhieu3012 <mjnkhieu3012@gmail.com>
2026-06-27 15:48:47 +0700

    update: lab 07

--- dist/lab/lab_07/lab_07.js
+++ dist/lab/lab_07/lab_07.js
@@ -1,1 +1,21 @@
-"use strict";
+const fetchBlogs = async () => {
+    const res = await fetch("http://localhost:8000/blogs");
+    const data = (await res.json());
+    //check lai hinh thu data (joi,zod,...)
+    if (data.length) {
+        const tbody = document.querySelector("#blogs tbody");
+        data.forEach((item, index) => {
+            tbody.innerHTML += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.title}</td>
+                <td>${item.author}</td>
+                <td>${item.content}</td>
+            </tr>
+        `;
+        });
+    }
+    console.log("check data:", data);
+};
+fetchBlogs();
+export {};
